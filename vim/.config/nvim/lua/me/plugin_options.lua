76718e10de9c6449ed19a9dfac58ffb8e99052c9
Ray Xue <xuerx@amazon.com>
2022-01-07 00:21:52 +0000

    Disable TS indent
    causing problem in python after :\n

--- vim/.config/nvim/lua/me/plugin_options.lua
+++ vim/.config/nvim/lua/me/plugin_options.lua
@@ -3,9 +3,9 @@ require("nvim-treesitter.configs").setup({
 	highlight = {
 		enable = true,
 	},
-	indent = {
-		enable = true,
-	},
+	-- indent = {
+	-- 	enable = true,
+	-- },
 	textobjects = {
 		select = {
 			enable = true,
