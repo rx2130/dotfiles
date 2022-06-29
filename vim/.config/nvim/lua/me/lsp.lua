018b509a7c6d87f6ee49437618b5eee57b7fbcac
Ray Xue <xuerx@amazon.com>
2022-06-29 01:49:06 +0000

    Not adding bemol to LSP workspace foldes

--- vim/.config/nvim/lua/me/lsp.lua
+++ vim/.config/nvim/lua/me/lsp.lua
@@ -116,13 +116,11 @@ function M.start_jdt()
 	local home = os.getenv("HOME")
 	local eclipse_workspace = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
 
-	local ws_folders_lsp = {}
 	local ws_folders_jdtls = {}
 	if root_dir then
 		local file = io.open(root_dir .. "/.bemol/ws_root_folders", "r")
 		if file then
 			for line in file:lines() do
-				table.insert(ws_folders_lsp, line)
 				table.insert(ws_folders_jdtls, string.format("file://%s", line))
 			end
 			file:close()
@@ -181,10 +179,6 @@ function M.start_jdt()
 	}
 
 	require("jdtls").start_or_attach(config)
-
-	for _, line in ipairs(ws_folders_lsp) do
-		vim.lsp.buf.add_workspace_folder(line)
-	end
 end
 
 return M
