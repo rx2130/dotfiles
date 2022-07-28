d9fcad580bdbed066c182abaaa515baf4db492fc
Ray Xue <xuerx@amazon.com>
2022-07-28 20:13:22 +0000

    Launch jdtls via included wrapper script

--- vim/.config/nvim/ftplugin/java.lua
+++ vim/.config/nvim/ftplugin/java.lua
@@ -39,29 +39,13 @@ local function start_jdt()
 		end
 	end
 
-	local system_name = vim.fn.has("mac") == 1 and "mac" or "linux"
 	local config = {
 		on_attach = jdtls_on_attach,
 		cmd = {
-			"java",
-			"-Declipse.application=org.eclipse.jdt.ls.core.id1",
-			"-Dosgi.bundles.defaultStartLevel=4",
-			"-Declipse.product=org.eclipse.jdt.ls.core.product",
-			"-Dlog.protocol=true",
-			"-Dlog.level=ALL",
-			"-Xms1g",
-			"-javaagent:" .. home .. "/Developer/lombok.jar",
-			"-jar",
-			vim.fn.glob(home .. "/.local/share/nvim/lsp_servers/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
-			"-configuration",
-			home .. "/.local/share/nvim/lsp_servers/jdtls/config_" .. system_name,
+			"jdtls",
+			"--jvm-arg=-javaagent:" .. home .. "/Developer/lombok.jar",
 			"-data",
 			eclipse_workspace,
-			"--add-modules=ALL-SYSTEM",
-			"--add-opens",
-			"java.base/java.util=ALL-UNNAMED",
-			"--add-opens",
-			"java.base/java.lang=ALL-UNNAMED",
 		},
 		root_dir = root_dir,
 		init_options = {
