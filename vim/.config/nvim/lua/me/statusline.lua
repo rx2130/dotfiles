67bf692264f5866db8abc1da3532a0c88e37404c
Ray Xue <xuerx@amazon.com>
2022-07-30 01:21:04 +0000

    Add space between statusline components

--- vim/.config/nvim/lua/me/statusline.lua
+++ vim/.config/nvim/lua/me/statusline.lua
@@ -7,7 +7,7 @@ function M.setup()
 
 		[[%{luaeval("require'me.statusline'.file_or_lsp_status()")}]],
 
-		"%m%r%=",
+		" %m%r%=",
 
 		"%#warningmsg#",
 		"%{&paste?'[paste] ':''}",
@@ -50,11 +50,21 @@ function M.dap_status()
 	return ""
 end
 
+local function format_uri(uri)
+	if vim.startswith(uri, "jdt://") then
+		local package = uri:match("contents/[%a%d._-]+/([%a%d._-]+)") or ""
+		local class = uri:match("contents/[%a%d._-]+/[%a%d._-]+/([%a%d$]+).class") or ""
+		return string.format("%s::%s", package, class)
+	else
+		return vim.fn.fnamemodify(vim.uri_to_fname(uri), ":.")
+	end
+end
+
 function M.file_or_lsp_status()
 	local messages = vim.lsp.util.get_progress_messages()
 	local mode = api.nvim_get_mode().mode
 	if mode ~= "n" or vim.tbl_isempty(messages) then
-		return M.format_uri(vim.uri_from_bufnr(0))
+		return format_uri(vim.uri_from_bufnr(0))
 	end
 	local percentage
 	local result = {}
@@ -72,16 +82,6 @@ function M.file_or_lsp_status()
 		return string.format("%03d%%: %s", percentage, table.concat(result, ", "))
 	else
 		return table.concat(result, ", ")
-	end
-end
-
-function M.format_uri(uri)
-	if vim.startswith(uri, "jdt://") then
-		local package = uri:match("contents/[%a%d._-]+/([%a%d._-]+)") or ""
-		local class = uri:match("contents/[%a%d._-]+/[%a%d._-]+/([%a%d$]+).class") or ""
-		return string.format("%s::%s", package, class)
-	else
-		return vim.fn.fnamemodify(vim.uri_to_fname(uri), ":.")
 	end
 end
 
