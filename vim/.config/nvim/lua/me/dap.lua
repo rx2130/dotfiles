db3abaf16065ba76759e9143f9c4053df30bbff6
Ray Xue <xuerx@amazon.com>
2022-08-03 04:50:52 +0000

    Fix dap-go config

--- vim/.config/nvim/lua/me/dap.lua
+++ vim/.config/nvim/lua/me/dap.lua
@@ -12,7 +12,37 @@ dap.configurations.java = {
 
 require("dap-python").setup()
 
-require("dap-go").setup()
+dap.adapters.delve = {
+	type = "server",
+	port = "${port}",
+	executable = {
+		command = "dlv",
+		args = { "dap", "-l", "127.0.0.1:${port}" },
+	},
+}
+
+dap.configurations.go = {
+	{
+		type = "delve",
+		name = "Debug",
+		request = "launch",
+		program = "${file}",
+	},
+	{
+		type = "delve",
+		name = "Debug test",
+		request = "launch",
+		mode = "test",
+		program = "${file}",
+	},
+	{
+		type = "delve",
+		name = "Debug test (go.mod)",
+		request = "launch",
+		mode = "test",
+		program = "./${relativeFileDirname}",
+	},
+}
 
 dap.adapters.lldb = {
 	type = "executable",
