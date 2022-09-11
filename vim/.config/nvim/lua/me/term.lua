2d18ef1a474e68a826f93722be841175cfec74c7
Ray Xue <xuerx@amazon.com>
2022-09-11 13:19:57 -0700

    Change toggle term buffer to use the whole window

--- vim/.config/nvim/lua/me/term.lua
+++ vim/.config/nvim/lua/me/term.lua
@@ -5,27 +5,19 @@ local winid = nil
 local bufid = nil
 
 local function launch_term()
-	vim.cmd("new")
+	vim.cmd("enew")
 	winid = vim.fn.win_getid()
 	bufid = vim.api.nvim_win_get_buf(0)
 	vim.fn.termopen(vim.fn.environ()["SHELL"])
 end
 
 local function open_term()
-	vim.cmd("new")
-	local tmpbuf = vim.api.nvim_win_get_buf(0)
-	winid = vim.fn.win_getid()
-	api.nvim_win_set_buf(winid, bufid)
-	api.nvim_buf_delete(tmpbuf, { force = true })
+	vim.cmd("buffer " .. bufid)
 end
 
+local key = api.nvim_replace_termcodes("<C-^>", true, false, true)
 local function hide_term()
-	if api.nvim_win_is_valid(winid) then
-		if #api.nvim_tabpage_list_wins(0) > 1 then
-			api.nvim_win_hide(winid)
-		end
-	end
-	winid = nil
+	vim.api.nvim_feedkeys(key, "n", true)
 end
 
 function M.toggle()
