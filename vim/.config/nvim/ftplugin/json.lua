5d562b917b5c8a0cb1ac85b00a9d6cb8561d655c
Ray Xue <xuerx@amazon.com>
2023-01-22 14:19:55 -0800

    Fix json lsp server cmd

--- vim/.config/nvim/ftplugin/json.lua
+++ vim/.config/nvim/ftplugin/json.lua
@@ -1,7 +1,7 @@
 vim.bo.formatprg = "python3 -m json.tool"
 
 vim.lsp.start({
-    name = "vscode-json-language-server",
-    cmd = { "vscode-json-language-server", "--stdio" },
+    name = "vscode-json-languageserver",
+    cmd = { "vscode-json-languageserver", "--stdio" },
     capabilities = require("cmp_nvim_lsp").default_capabilities(),
 })
