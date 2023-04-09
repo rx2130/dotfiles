562a8abcb7828bedcbf7d94291e6beb888560aa2
Ray Xue <xuerx@amazon.com>
2023-04-09 10:43:03 -0700

    Add js/ts dap support

--- vim/.config/nvim/ftplugin/javascript.lua
+++ vim/.config/nvim/ftplugin/javascript.lua
@@ -1,8 +1,30 @@
 vim.bo.formatprg = "prettier --parser typescript"
 
 vim.lsp.start({
-    name = "tsserver",
-    cmd = { "typescript-language-server", "--stdio" },
-    root_dir = vim.fs.dirname(vim.fs.find({ ".git", "package.json" }, { upward = true })[1]),
-    capabilities = require("cmp_nvim_lsp").default_capabilities(),
+	name = "tsserver",
+	cmd = { "typescript-language-server", "--stdio" },
+	root_dir = vim.fs.dirname(vim.fs.find({ ".git", "package.json" }, { upward = true })[1]),
+	capabilities = require("cmp_nvim_lsp").default_capabilities(),
 })
+
+local dap = require("dap")
+
+dap.adapters["pwa-node"] = {
+	type = "server",
+	host = "localhost",
+	port = "${port}",
+	executable = {
+		command = "node",
+		args = { "/Users/xuerx/Developer/vscode-js-debug/dist/src/dapDebugServer.js", "${port}" },
+	},
+}
+
+dap.configurations.javascript = {
+	{
+		type = "pwa-node",
+		request = "launch",
+		name = "Launch file",
+		program = "${file}",
+		cwd = "${workspaceFolder}",
+	},
+}
--- vim/.config/nvim/ftplugin/typescript.lua
+++ vim/.config/nvim/ftplugin/typescript.lua
@@ -1,8 +1,59 @@
 vim.bo.formatprg = "prettier --parser typescript"
 
 vim.lsp.start({
-    name = "tsserver",
-    cmd = { "typescript-language-server", "--stdio" },
-    root_dir = vim.fs.dirname(vim.fs.find({ ".git", "package.json" }, { upward = true })[1]),
-    capabilities = require("cmp_nvim_lsp").default_capabilities(),
+	name = "tsserver",
+	cmd = { "typescript-language-server", "--stdio" },
+	root_dir = vim.fs.dirname(vim.fs.find({ ".git", "package.json" }, { upward = true })[1]),
+	capabilities = require("cmp_nvim_lsp").default_capabilities(),
 })
+
+local dap = require("dap")
+
+dap.adapters["pwa-node"] = {
+	type = "server",
+	host = "localhost",
+	port = "${port}",
+	executable = {
+		command = "node",
+		args = { "/Users/xuerx/Developer/vscode-js-debug/dist/src/dapDebugServer.js", "${port}" },
+	},
+}
+
+dap.configurations.typescript = {
+	{
+		type = "pwa-node",
+		request = "launch",
+		name = "Launch file",
+		program = "${file}",
+		cwd = "${workspaceFolder}",
+		runtimeExecutable = "ts-node",
+		sourceMaps = true,
+		protocol = "inspector",
+		-- console = "integratedTerminal",
+		resolveSourceMapLocations = {
+			"${workspaceFolder}/dist/**/*.js",
+			"${workspaceFolder}/**",
+			"!**/node_modules/**",
+		},
+	},
+	{
+		type = "pwa-node",
+		request = "launch",
+		name = "Debug Jest Tests",
+		-- trace = true, -- include debugger info
+		runtimeExecutable = "node",
+		runtimeArgs = {
+			"./node_modules/jest/bin/jest.js",
+			"--runInBand",
+		},
+		rootPath = "${workspaceFolder}",
+		cwd = "${workspaceFolder}",
+		console = "integratedTerminal",
+		internalConsoleOptions = "neverOpen",
+		resolveSourceMapLocations = {
+			"${workspaceFolder}/dist/**/*.js",
+			"${workspaceFolder}/**",
+			"!**/node_modules/**",
+		},
+	},
+}
