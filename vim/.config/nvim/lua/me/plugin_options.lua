1ef947129220bb261b9767740d6daa9c49a46ab8
Ray Xue <xuerx@amazon.com>
2023-10-21 18:17:21 +0000

    Replace luasnip with vim.snippet

--- vim/.config/nvim/lua/me/plugin_options.lua
+++ vim/.config/nvim/lua/me/plugin_options.lua
@@ -72,12 +72,11 @@ require("nvim-treesitter.configs").setup({
 })
 
 -- completion
-local luasnip = require("luasnip")
 local cmp = require("cmp")
 cmp.setup({
 	snippet = {
 		expand = function(args)
-			luasnip.lsp_expand(args.body)
+			vim.snippet.expand(args.body)
 		end,
 	},
 	mapping = {
