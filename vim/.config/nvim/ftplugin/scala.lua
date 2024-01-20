7ac76f06b04a668ba84fb8f97b156e7850e1a3b8
Ray Xue <xueruimvp@gmail.com>
2024-01-19 17:57:47 -0800

    Add scala LSP config

--- vim/.config/nvim/ftplugin/scala.lua
+++ vim/.config/nvim/ftplugin/scala.lua
@@ -1,1 +1,31 @@
-require("metals").initialize_or_attach({})
+local metals = require("metals")
+
+local metals_config = metals.bare_config()
+metals_config.init_options.statusBarProvider = "on"
+metals_config.settings = {
+	showImplicitArguments = true,
+	excludedPackages = { "--com.apple", "--apple" },
+}
+metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+metals.initialize_or_attach(metals_config)
+
+local dap = require("dap")
+dap.configurations.scala = {
+	{
+		type = "scala",
+		request = "launch",
+		name = "Run or test",
+		metals = {
+			runType = "runOrTestFile",
+		},
+	},
+	{
+		type = "scala",
+		request = "launch",
+		name = "Test build target",
+		metals = {
+			runType = "testTarget",
+		},
+	},
+}
