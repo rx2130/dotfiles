8fc247a18d531da22aa91dc2cccd0f0956b6ada9
Ray Xue <xueruimvp@gmail.com>
2024-03-29 18:10:04 -0700

    vim: Replace gitsigns.nvim with mini.diff

--- vim/.config/nvim/lua/me/plugin_options.lua
+++ vim/.config/nvim/lua/me/plugin_options.lua
@@ -170,6 +170,18 @@ require("mini.surround").setup({
 	search_method = "cover_or_next",
 })
 
+require("mini.diff").setup({
+	view = {
+		signs = { add = "│", change = "│", delete = "_" },
+	},
+	mappings = {
+		goto_first = "[C",
+		goto_prev = "[c",
+		goto_next = "]c",
+		goto_last = "]C",
+	},
+})
+
 -- Remap adding surrounding to Visual mode selection
 vim.api.nvim_del_keymap("x", "ys")
 vim.api.nvim_set_keymap("x", "S", [[:<C-u>lua MiniSurround.add('visual')<CR>]], { noremap = true })
@@ -311,56 +323,5 @@ require("nvim-tree").setup({
 		local api = require("nvim-tree.api")
 		api.config.mappings.default_on_attach(bufnr)
 		vim.keymap.set("n", "<C-s>", api.node.open.horizontal)
-	end,
-})
-
-require("gitsigns").setup({
-	on_attach = function(bufnr)
-		local gs = package.loaded.gitsigns
-
-		local function map(mode, l, r, opts)
-			opts = opts or {}
-			opts.buffer = bufnr
-			vim.keymap.set(mode, l, r, opts)
-		end
-
-		-- Navigation
-		map("n", "]c", function()
-			if vim.wo.diff then
-				return "]c"
-			end
-			vim.schedule(function()
-				gs.next_hunk()
-			end)
-			return "<Ignore>"
-		end, { expr = true })
-
-		map("n", "[c", function()
-			if vim.wo.diff then
-				return "[c"
-			end
-			vim.schedule(function()
-				gs.prev_hunk()
-			end)
-			return "<Ignore>"
-		end, { expr = true })
-
-		-- Actions
-		-- map('n', '<leader>hs', gs.stage_hunk)
-		-- map('n', '<leader>hr', gs.reset_hunk)
-		-- map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-		-- map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-		-- map('n', '<leader>hS', gs.stage_buffer)
-		-- map('n', '<leader>hu', gs.undo_stage_hunk)
-		-- map('n', '<leader>hR', gs.reset_buffer)
-		-- map('n', '<leader>hp', gs.preview_hunk)
-		-- map('n', '<leader>hb', function() gs.blame_line{full=true} end)
-		-- map('n', '<leader>tb', gs.toggle_current_line_blame)
-		-- map('n', '<leader>hd', gs.diffthis)
-		-- map('n', '<leader>hD', function() gs.diffthis('~') end)
-		-- map('n', '<leader>td', gs.toggle_deleted)
-
-		-- -- Text object
-		-- map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
 	end,
 })
