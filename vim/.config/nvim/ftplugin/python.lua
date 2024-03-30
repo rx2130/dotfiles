0b0c0f07c5a20c27e4a8ca547e49558e2a56f5ac
Ray Xue <xueruimvp@gmail.com>
2024-03-29 18:23:35 -0700

    vim: replace pylsp with pyright

--- vim/.config/nvim/ftplugin/python.lua
+++ vim/.config/nvim/ftplugin/python.lua
@@ -2,25 +2,15 @@ vim.bo.makeprg = "python3 %"
 
 vim.bo.formatprg = "black --quiet -"
 
-if vim.fn.executable("pylsp") == 0 then
+if vim.fn.executable("pyright-langserver") == 0 then
 	return
 end
 
 vim.lsp.start({
-	name = "pylsp",
-	cmd = { "pylsp" },
+	name = "pyright",
+	cmd = { "pyright-langserver", "--stdio" },
 	root_dir = vim.fs.dirname(vim.fs.find({ "requirements.txt", "setup.py", "setup.cfg" }, { upward = true })[1]),
 	capabilities = require("cmp_nvim_lsp").default_capabilities(),
-	settings = {
-		pylsp = {
-			plugins = {
-				pycodestyle = {
-					ignore = { "E101", "E501" },
-					-- maxLineLength = 100,
-				},
-			},
-		},
-	},
 })
 
 local dap = require("dap")
