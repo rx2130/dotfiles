29688194131892c7845ee725afe66b3ff23b8724
Ray Xue <xueruimvp@gmail.com>
2025-07-03 13:04:48 -0700

    vim: update tex lsp

--- vim/.config/nvim/ftplugin/tex.lua
+++ vim/.config/nvim/ftplugin/tex.lua
@@ -8,24 +8,19 @@ vim.lsp.start({
 	name = "texlab",
 	cmd = { "texlab" },
 	-- cmd = {
-	-- 	"/Users/xuerx/Developer/texlab/target/debug/texlab",
+	-- 	"/opt/homebrew/bin/texlab",
 	-- 	"-vvvv",
 	-- 	"--log-file",
-	-- 	"/Users/xuerx/Documents/resume/texlab.log",
+	-- 	"/Users/xuerx/Developer/resume/texlab.log",
 	-- },
 	capabilities = require("cmp_nvim_lsp").default_capabilities(),
 	settings = {
-		build = {
-			executable = "tectonic",
-			args = {
-				"-X",
-				"compile",
-				"%f",
-				"--synctex",
-				"--keep-logs",
-				"--keep-intermediates",
+		texlab = {
+			build = {
+				executable = "tectonic",
+				args = { "%f" },
+				onSave = true,
 			},
-			onSave = true,
 		},
 	},
 })
@@ -39,20 +34,16 @@ local texlab_build_status = vim.tbl_add_reverse_lookup({
 
 local function buf_build()
 	local bufnr = 0
-	local texlab_client = vim.lsp.get_active_clients({ bufnr = bufnr, name = "texlab" })[1]
+	local client = vim.lsp.get_clients({ bufnr = bufnr, name = "texlab" })[1]
 	local params = {
 		textDocument = { uri = vim.uri_from_bufnr(bufnr) },
 	}
-	if texlab_client then
-		texlab_client.request("textDocument/build", params, function(err, result)
-			if err then
-				error(tostring(err))
-			end
-			print("Build " .. texlab_build_status[result.status])
-		end, bufnr)
-	else
-		print("method textDocument/build is not supported by any servers active on the current buffer")
-	end
+	client.request("textDocument/build", params, function(err, result)
+		if err then
+			error(tostring(err))
+		end
+		vim.notify("Build " .. texlab_build_status[result.status])
+	end, bufnr)
 end
 
 vim.api.nvim_buf_create_user_command(0, "TexlabBuild", buf_build, {})
